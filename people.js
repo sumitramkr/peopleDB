7070c9ef4592f60669eb9f450a6f2e62bae9fcae
sumitramkr <sumitramkr77e@gmail.com>
2022-06-17 00:01:12 +0530

    updated speelings

--- people.js
+++ people.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/peoplesDB", {
+mongoose.connect("mongodb://localhost:27017/peopleDB", {
   useNewUrlParser: true,
 });
 
