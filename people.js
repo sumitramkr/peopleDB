faf93e3da9ab00d4bac623c214fe5e4322075688
sumitramkr <sumitramkr77e@gmail.com>
2022-06-17 10:59:33 +0530

    logged error

--- people.js
+++ people.js
@@ -16,4 +16,62 @@ const people = new People({
   age: 37,
 });
 
-people.save();
+const fruitSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "name please"],
+  },
+  ratings: {
+    type: Number,
+    min: 1,
+    max: 10,
+  },
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit({
+  // name: "Apple",
+  ratings: 10,
+});
+
+const orange = new Fruit({
+  name: "Orange",
+  ratings: 10,
+});
+
+const banana = new Fruit({
+  name: "Banana",
+  ratings: 10,
+});
+
+const guava = new Fruit({
+  name: "Guava",
+  ratings: 9,
+});
+
+// Fruit.insertMany([orange, banana, guava], function (err) {
+//   if (err) {
+//     console.log("Error");
+//   } else {
+//     console.log("Success");
+//   }
+// });
+
+// people.save();
+
+fruit.save();
+
+//read data
+
+Fruit.find(function (err, fruits) {
+  console.log(fruits);
+  // if (err) {
+  //   console.log(err);
+  // } else {
+  //   mongoose.connection.close();
+  //   fruits.forEach(function (element) {
+  //     console.log(element.name);
+  //   });
+  // }
+});
